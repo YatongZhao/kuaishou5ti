93b4026f8aa883c47d61d973226d194acdfb83be
YatongZhao <zyt768295828@163.com>
2021-09-08 10:04:24 +0800

    添加对异步回掉的支持

--- 5.js
+++ 5.js
@@ -1,4 +1,7 @@
 // 7:55
+// 10:30
+
+// 9:31
 
 class Bus {
     eventMap = {};
@@ -16,43 +19,47 @@ class Bus {
     }
 
     triggerWithId(eventId, id) {
-        return eventName => this.trigger.call(this, eventName, {parentEventId: eventId, parentCbId: id});
+        return eventName => this.triggerCore.call(this, eventName, {parentEventId: eventId, parentCbId: id});
     }
 
-    trigger(eventName, {parentEventId, parentCbId}) {
+    trigger(eventName) {
+        return this.triggerCore(eventName);
+    }
+
+    triggerCore(eventName, parentIdMap) {
         let events = this.eventMap[eventName];
         if (!events) return;
         let eventId = this.eventId++;
 
-        this.beforeTriggerHooks.forEach(hook => hook(eventId, eventName));
+        this.beforeTriggerHooks.forEach(hook => hook({eventId, eventName, parentIdMap}));
 
         let count = events.length;
         events.forEach((cb, cbId) => {
-            this.beforeCallbackHooks.forEach(hook => hook(eventId, cbId, eventName, cb));
+            this.beforeCallbackHooks.forEach(hook => hook({eventId, cbId, eventName, cb, parentIdMap}));
             let res = cb.call({
                 trigger: this.triggerWithId(eventId, cbId)
             });
 
             if (res instanceof Promise) {
                 res.catch(err => {
-                    this.afterCallbackHooks.forEach(hook => hook(eventId, cbId, eventName, cb));
+                    this.afterCallbackHooks.forEach(hook => hook({eventId, cbId, eventName, cb, parentIdMap}));
                     count--;
 
-                    if (count === 0) this.afterTriggerHooks.forEach(hook => hook(eventId, eventName));
+                    if (count === 0) this.afterTriggerHooks.forEach(hook => hook({eventId, eventName, parentIdMap}));
                     throw err;
                 }).then(data => {
-                    this.afterCallbackHooks.forEach(hook => hook(eventId, cbId, eventName, cb));
+                    this.afterCallbackHooks.forEach(hook => hook({eventId, cbId, eventName, cb, parentIdMap}));
                     count--;
                     
-                    if (count === 0) this.afterTriggerHooks.forEach(hook => hook(eventId, eventName));
+                    if (count === 0) this.afterTriggerHooks.forEach(hook => hook({eventId, eventName, parentIdMap}));
                     return data;
                 });
             } else {
                 count--;
-                this.afterCallbackHooks.forEach(hook => hook(eventId, cbId, eventName, cb));
+                this.afterCallbackHooks.forEach(hook => hook({eventId, cbId, eventName, cb, parentIdMap}));
             }
         });
-        if (count === 0) this.afterTriggerHooks.forEach(hook => hook(eventId, eventName));
+        if (count === 0) this.afterTriggerHooks.forEach(hook => hook({eventId, eventName, parentIdMap}));
     }
 
     removeEventListener(eventName, cb) {
@@ -69,38 +76,73 @@ class Bus {
     }
 }
 
+
+
+class EventLog {
+    cb = [];
+    eventName;
+    eventId;
+
+    constructor(eventName, eventId) {
+        this.eventName = eventName;
+        this.eventId = eventId;
+    }
+
+    logStack(stackNum, cb) {
+        let result = '--'.repeat(stackNum) + this.eventName + '\n'
+            + this.cb.map(callBackLog => callBackLog.logStack(stackNum+1, cb)).join('');
+
+        if (cb) cb(this.eventId);
+
+        return result;
+    }
+
+    log(cb) {
+        return this.logStack(0, cb);
+    }
+}
+
+class CallBackLog {
+    event = [];
+    name;
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    logStack(stackNum, cb) {
+        let result = '--'.repeat(stackNum) + this.name + '\n'
+            + this.event.map(eventLog => eventLog.logStack(stackNum+1, cb)).join('');
+
+        return result;
+    }
+}
+
 function logger() {
-    let stackNum = 0;
-    let log = '';
-
-    function pushStack(str) {
-        log += '--'.repeat(stackNum) + str + '\n';
-        stackNum++;
-    }
-
-    function popStack() {
-        stackNum--;
-    }
-
+    let rootIdMap = {};
+    let eventMap = {};
     return {
-        beforeTrigger(eventId, eventName) {
-            pushStack('event: ' + eventName);
+        beforeTrigger({eventId, eventName, parentIdMap}) {
+            eventMap[eventId] = new EventLog(eventName, eventId);
+
+            if (!parentIdMap) {
+                rootIdMap[eventId] = true;
+            } else {
+                let { parentEventId, parentCbId } = parentIdMap;
+                eventMap[parentEventId].cb[parentCbId].event.push(eventMap[eventId]);
+            }
         },
-        beforeCallback(eventId, cbId, eventName, cb) {
-            pushStack('callback: ' + cb.name);
+        beforeCallback({eventId, cbId, eventName, cb, parentIdMap}) {
+            eventMap[eventId].cb[cbId] = new CallBackLog(cb.name);
         },
-        afterCallback(eventId, cbId, eventName, cb) {
-            popStack();
-        },
-        afterTrigger(eventId, eventName) {
-            popStack();
-            if (stackNum === 0) {
-                console.log(log);
-                log = '';
+        afterCallback({eventId, cbId, eventName, cb, parentIdMap}) { },
+        afterTrigger({eventId, eventName, parentIdMap}) {
+            if (rootIdMap[eventId]) {
+                console.log(eventMap[eventId].log((eventId) => delete eventMap[eventId]));
+                delete rootIdMap[eventId];
             }
         }
     }
-
 }
 
 const bus = new Bus()
@@ -113,23 +155,25 @@ const bus = new Bus()
 // bus.trigger('testEvent', 1, 2)
 // bus.trigger('testEvent', 1, 2)
 
-bus.addEventListener('testEvent', function callback1(){
-    console.log('testEvent')
+bus.addEventListener('testEvent', async function callback1(){
+    // console.log('testEvent')
     // do something
+    await Promise.resolve();
     this.trigger('testEvent2')
     this.trigger('testEvent2')
 })
-bus.addEventListener('testEvent', function callback1(){
-    console.log('testEvent')
+bus.addEventListener('testEvent', function callback3(){
+    // console.log('testEvent')
     // do something
     this.trigger('testEvent2')
 })
 
 
 bus.addEventListener('testEvent2', function callback2(){
-    console.log('testEvent2')
+    // console.log('testEvent2')
     // do something
 })
 
 bus.use(logger());
+bus.trigger('testEvent');
 bus.trigger('testEvent');
